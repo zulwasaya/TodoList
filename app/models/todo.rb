8f8af404a3eb54c95454e1ac8d1f3267d51ccffa
Zul Wasaya <zul.wasaya@gmail.com>
2013-06-03 18:44:34 +0100

    removed validation of duplicate subject

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,7 +1,7 @@
 class Todo < ActiveRecord::Base
   attr_accessible :description, :priority, :subject, :user
   belongs_to :user
-  validates_uniqueness_of :subject, message:"Todo must be unique"
+
   validates_presence_of :subject
   validates_presence_of :priority
   validates_numericality_of :priority
--- app/models/todolist.rb
+++ app/models/todolist.rb
@@ -1,6 +1,7 @@
 class Todolist < ActiveRecord::Base
   attr_accessible :priority, :task
   validates_presence_of :task
+
   validates_presence_of :priority
   belongs_to :todo
 end
