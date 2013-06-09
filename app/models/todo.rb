d77d5f95d424c9d0e71bf568435f5d466a421224
Zul Wasaya <zul.wasaya@gmail.com>
2013-06-09 17:27:28 +0100

    changed not to have to input priority for tasks and subjects, defualts to 1

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,8 +3,7 @@ class Todo < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :subject
-  validates_presence_of :priority
-  validates_numericality_of :priority
+
   validates_presence_of :description
   has_many :todolists
 end
